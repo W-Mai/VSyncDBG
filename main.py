b452367cc1c342e896dcd20bde1a3371df95cbed
W-Mai <1341398182@qq.com>
2023-01-17 13:39:23 +0800

    update

--- main.py
+++ main.py
@@ -1,74 +1,64 @@
-import threading
-import time
+class Machine(object):
+    def __init__(self):
+        self.millis = 0.0
+        self._timestamps = {}
+        self._signals = {}
 
-poll_flag = 0
-buffer_index = 0
-send_index = 1
-mipi_busy_flag = 0
-render_flag = 0
+    def update(self, delta: float):
+        self.millis += delta
 
-sync_sig = True
+        self._update_lcd_c()
+        self._update_render()
 
+    def _get_time(self, key):
+        if not self._timestamps.get(key, None):
+            self._timestamps[key] = self.millis
+        return self._timestamps[key]
 
-def wait(t):
-    time.sleep(t / 1000)
+    def _set_time(self, key, val):
+        self._timestamps[key] = val
 
+    def _set_current_time(self, key):
+        self._timestamps[key] = self.millis
 
-def te_intr():
-    global poll_flag, send_index, buffer_index, mipi_busy_flag
-    poll_flag = 1
-    send_index = buffer_index
+    def _get_signal(self, key):
+        if not self._signals.get(key, None):
+            self._signals[key] = 0
+        return self._signals[key]
 
-    mipi_busy_flag = 1
+    def _set_signal(self, key, val):
+        self._signals[key] = val
 
+    def _update_lcd_c(self):
+        if self._get_signal("lcd_c") == 1:
+            if self.millis - self._get_time("lcd_c") > 35:
+                self._set_signal("lcd_c", 0)
+                self._set_current_time("lcd_c")
+        else:
+            if self.millis - self._get_time("lcd_c") > 7:
+                self._set_signal("lcd_c", 1)
+                self._set_signal("poll", 1)
+                self._set_current_time("lcd_c")
 
-def frame_done():
-    global mipi_busy_flag
-    mipi_busy_flag = 0
+    def _update_render(self):
+        if self._get_signal("poll"):
+            self._set_signal("poll", 0)
+            self._set_signal("render", 1)
 
+        if self._get_signal("render") == 1:
+            if self.millis - self._get_time("render") > 15:
+                self._set_signal("render", 0)
+                self._set_current_time("render")
 
-def loop_lcd_c():
-    while sync_sig:
-        te_intr()
-        wait(35)
-        frame_done()
-        wait(7)
+    # call
+    def _te_intr(self):
+        pass
 
-
-def loop_render():
-    global poll_flag, buffer_index, render_flag
-    while sync_sig:
-        if poll_flag:
-            poll_flag = 0
-        render_flag = 1
-        wait(15)  # render to buffer
-        buffer_index = 1 - buffer_index
-        render_flag = 0
-
-
-def loop_probe():
-    global sync_sig
-    global poll_flag, buffer_index, render_flag
-    count = 0
-    try:
-        with open("log", "w") as f:
-            while sync_sig:
-                if count == 80000:
-                    sync_sig = False
-                f.write(f"{poll_flag} {buffer_index} {send_index} {mipi_busy_flag} {render_flag}\n")
-                print(render_flag)
-                wait(0.1)
-                count += 1
-    except Exception:
-        f.close()
+    def _frame_done(self):
+        pass
 
 
-l_lcd_t = threading.Thread(target=loop_lcd_c)
-l_render_t = threading.Thread(target=loop_render)
-l_probe_t = threading.Thread(target=loop_probe)
-
-l_lcd_t.start()
-l_render_t.start()
-l_probe_t.start()
-
-l_probe_t.join()
+m = Machine()
+for i in range(100):
+    m.update(1)
+    print(m._signals)
