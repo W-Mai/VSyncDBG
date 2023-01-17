2f5f1ea56bbed3a4c76f4df424b6962c336c0323
W-Mai <1341398182@qq.com>
2023-01-17 14:19:15 +0800

    update
    Signed-off-by: W-Mai <1341398182@qq.com>

--- draw.py
+++ draw.py
@@ -1,27 +1,16 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
-with open("log") as f:
-    lines = list(map(lambda x: x.split(), f.readlines()))
+with open("log2") as f:
+    lines = list(map(lambda x0: x0.split(), f.readlines()))
     arr = np.array(lines, dtype="int32")
-    size = arr.shape[0]
+    size, row = arr.shape
 
 x = np.arange(size)
-poll_flag = arr[:, 0]
-buffer_index = arr[:, 1]
-send_index = arr[:, 2]
-mipi_flag = arr[:, 3]
-render_flag = arr[:, 4]
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(nrows=row)
 
-ax.step(x, poll_flag, linewidth=2.5)
-ax.step(x, buffer_index + 1, linewidth=2.5)
-ax.step(x, send_index + 2, linewidth=2.5)
-ax.step(x, mipi_flag + 3, linewidth=2.5)
-ax.step(x, render_flag + 4, linewidth=2.5)
-
-# ax.set(ylim=(0, 1), yticks=np.arange(0, 2))
-
+for i in range(row):
+    ax[i].step(x, arr[:, i], linewidth=2.5)
 
 plt.show()
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 class Machine(object):
-    def __init__(self):
+    def __init__(self, fd):
         self.millis = 0.0
         self._timestamps = {}
         self._signals = {}
+
+        self.fd = fd
 
     def update(self, delta: float):
         self.millis += delta
@@ -13,11 +15,13 @@ class Machine(object):
         print(self.dump_sig())
 
     def dump_sig(self):
+        keys = ['lcd_c', 'poll', 'render', 'buffer', 'send', 'mipi_busy']
+        self.fd.write(" ".join(map(str, [
+            self._signals.get(key, 0) for key in keys
+        ])) + "\n")
+
         return {
-            key: self._signals.get(key, 0) for key in [
-                'lcd_c', 'poll', 'render',
-                'buffer', 'send', 'mipi_busy'
-            ]
+            key: self._signals.get(key, 0) for key in keys
         }
 
     def _get_time(self, key):
@@ -43,6 +47,7 @@ class Machine(object):
         if self._get_signal("lcd_c") == 1:
             if self.millis - self._get_time("lcd_c") > 35:
                 self._set_signal("lcd_c", 0)
+                self._frame_done()
                 self._set_current_time("lcd_c")
         else:
             if self.millis - self._get_time("lcd_c") > 7:
@@ -54,12 +59,13 @@ class Machine(object):
         if self._get_signal("poll"):
             self._set_signal("poll", 0)
             self._set_signal("render", 1)
+            self._set_current_time("render")
 
         if self._get_signal("render") == 1:
             if self.millis - self._get_time("render") > 15:
                 self._set_signal("render", 0)
                 self._set_signal("buffer", 1 - self._get_signal("buffer"))
-                self._set_current_time("render")
+                # self._set_current_time("render")
 
     # call
     def _te_intr(self):
@@ -71,6 +77,9 @@ class Machine(object):
         self._set_signal("mipi_busy", 0)
 
 
-m = Machine()
-for i in range(100):
-    m.update(1)
+with open("log2", "w") as f:
+    m = Machine(f)
+    for i in range(200):
+        m.update(1)
+
+import draw
