e531cbf5ce967dda4ce25ec3a6517fb26ff5161c
FASTSHIFT <vifextech@foxmail.com>
2023-06-29 22:42:59 +0800

    update crash

--- main.py
+++ main.py
@@ -5,86 +5,108 @@ from random import randrange
 
 
 class Prj(object):
-    POLL_LIMIT = 2
-    VSYNC_QUEUE = []
+    INITED = False
 
     @staticmethod
-    def update_lcd_c(m: Machine):
-        lcd_c = m.get_signal("lcd_c")
+    def on_init(m: Machine):
+        pollready = m.get_signal("pollready")
+        pollmon = m.get_signal("pollmon")
+        
+        if not Prj.INITED:
+            pollready.set(1)
+            pollmon.set(1)
+            Prj.INITED = True
 
-        if lcd_c.get() == 0:
-            if not lcd_c.keeping(7):
-                lcd_c.set(1)
+    @staticmethod
+    def update_lcd(m: Machine):
+        lcd = m.get_signal("lcd")
+        send_buffer = m.get_signal("send_buffer")
+
+        if lcd.get() == 0:
+            if not lcd.keeping(7):
+                lcd.set(1)
                 Prj.te_intr(m)
+                    
         else:
-            if not lcd_c.keeping(8):
-                lcd_c.set(0)
-                Prj.frame_done(m)
+            if not lcd.keeping(8):
+                if send_buffer.get():
+                    Prj.frame_done(m)
+                lcd.set(0)
 
     @staticmethod
     def update_render(m: Machine):
-        poll = m.get_signal("poll")
+        pollready = m.get_signal("pollready")
+        pollmon = m.get_signal("pollmon")
         render = m.get_signal("render")
         render_buffer = m.get_signal("render_buffer")
+        commit_buffer = m.get_signal("commit_buffer")
 
-        if poll.get() and render.get() != 1:
+        if pollmon.get() and pollready.get() and render.get() != 1:
+            pollmon.set(0)
             render.set(1)
-            Prj.check_collision(m)
+            Prj.check_crruption(m)
             render.update_time()
 
         if render.get() == 1:
             rnd = randrange(2, 100)
             # rnd = 20
             if not render.keeping(rnd):
-                Prj.check_collision(m)
+                Prj.check_crruption(m)
                 
-                Prj.VSYNC_QUEUE.append(render_buffer.get())
-                poll.set(poll.get() - 1)
+                commit_buffer.set(render_buffer.get())
                 render_buffer.set(1 - render_buffer.get())
                 render.set(0)
+                pollmon.set(1)
 
     @staticmethod
     def te_intr(m: Machine):
-        poll = m.get_signal("poll")
+        commit_buffer = m.get_signal("commit_buffer")
+        if(commit_buffer.get()):
+            Prj.frame_start(m)
+
+    @staticmethod
+    def frame_start(m: Machine):
         send_buffer = m.get_signal("send_buffer")
+        commit_buffer = m.get_signal("commit_buffer")
 
-        send_buffer.set(Prj.VSYNC_QUEUE.pop(0) if Prj.VSYNC_QUEUE else - 1)
-        Prj.check_collision(m)
-
-        poll.set(poll.get() + 1
-                 if poll.get() < Prj.POLL_LIMIT
-                 else Prj.POLL_LIMIT)
+        send_buffer.set(commit_buffer)
+        Prj.check_crruption(m)
 
     @staticmethod
     def frame_done(m: Machine):
-        Prj.check_collision(m)
+        pollready = m.get_signal("pollready")
         send_buffer = m.get_signal("send_buffer")
+        Prj.check_crruption(m)
+        pollready.set(1)
         send_buffer.set(-1)
 
     @staticmethod
-    def check_collision(m: Machine):
+    def check_crruption(m: Machine):
         send_buffer = m.get_signal("send_buffer")
         render_buffer = m.get_signal("render_buffer")
-        collision = m.get_signal("collision")
+        crruption = m.get_signal("crruption")
         render = m.get_signal("render")
-        lcd_c = m.get_signal("lcd_c")
+        lcd = m.get_signal("lcd")
 
-        if render.get() and lcd_c.get() and send_buffer.get() == render_buffer.get():
-            collision.set(collision.get() + 1)
-            print("Collision detected!")
+        if render.get() and lcd.get() and send_buffer.get() == render_buffer.get():
+            crruption.set(crruption.get() + 1)
+            print("crruption detected!")
 
 if __name__ == '__main__':
     with StringIO() as f:
         machine = Machine(f, dump_signals=[
-            'poll',
-            'lcd_c',
+            'pollready',
+            'pollmon',
+            'lcd',
             'render',
             'render_buffer',
             'send_buffer',
-            'collision'
+            'commit_buffer',
+            'crruption'
         ], tick_per_mil=1)
 
-        machine.add_updater(Prj.update_lcd_c)
+        machine.add_updater(Prj.on_init)
+        machine.add_updater(Prj.update_lcd)
         machine.add_updater(Prj.update_render)
 
         for i in range(int(machine.calc_sim_time(200))):
