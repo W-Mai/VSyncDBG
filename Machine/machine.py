901e404606654c2b0d393eed89caf2950009e25a
W-Mai <1341398182@qq.com>
2023-06-30 01:54:32 +0800

    😁(core): refactor new structure

--- Machine/machine.py
+++ Machine/machine.py
@@ -1,7 +1,7 @@
 from io import StringIO
 
 
-class Signal(object):
+class _Signal(object):
     def __init__(self, machine: 'Machine', name: str):
         self._name = name
         self._machine = machine
@@ -37,7 +37,7 @@ class Signal(object):
         self._machine._set_current_time(self._name)
 
 
-class SignalWrapper(object):
+class SignalsWrapper(object):
     def __init__(self, machine: 'Machine'):
         self._machine = machine
 
@@ -48,22 +48,21 @@ class SignalWrapper(object):
         return self._machine._signals_obj
 
     def __dir__(self):
-        return list(self.__dict__().keys()) + dir(type(self)) + ["fuck"]
+        return list(self.__dict__().keys()) + dir(type(self))
 
 
 class Machine(object):
-    def __init__(self, fd, dump_signals, tick_per_mil):
+    def __init__(self, dump_signals):
         self.millis = 0.0
         self._timestamps = {}
         self._signals = {}
         self._signals_obj = {}
         self._dump_signals = dump_signals
         self._updaters = []
-        self._tick_per_mil = tick_per_mil
+        self._tick_per_mil = 0
+        self.fd = None
 
-        self.s = SignalWrapper(self)
-
-        self.fd = fd
+        self.s = SignalsWrapper(self)
 
     def calc_sim_time(self, t):
         return t * self._tick_per_mil
@@ -114,15 +113,79 @@ class Machine(object):
     # Public Functions
     def add_updater(self, updater):
         self._updaters.append(updater)
-        self.fd.seek(0)
-        self._dump_sig_header()
 
     def get_signal(self, name):
+        if name not in self._dump_signals:
+            raise Exception(f"{name} is not in dump signals")
+
         sig = self._signals_obj.get(name, None)
         if not sig:
-            sig = Signal(self, name)
+            sig = _Signal(self, name)
             self._signals_obj[name] = sig
         return sig
+
+
+class Signal(object):
+    def __init__(self, init_val):
+        self.init_val = init_val
+
+
+class Updater(object):
+    def __init__(self, updater):
+        self.updater = updater
+        self.project = None
+
+    def __call__(self, machine):
+        # machine: not use
+        return self.updater(self.project)
+
+    def set_project(self, project):
+        self.project = project
+
+    @property
+    def is_updater(self):
+        return True
+
+
+class ProjectMeta(type):
+    def __new__(mcs, name, bases, attrs):
+        dump_signals = []
+        new_attrs = {}
+        for key, val in attrs.items():
+            if isinstance(val, Signal):
+                dump_signals.append(key)
+            else:
+                new_attrs[key] = val
+
+        machine = Machine(dump_signals)
+        for sig in dump_signals:
+            machine.get_signal(sig)(attrs[sig].init_val)
+
+        new_attrs["_machine"] = machine
+        new_attrs["s"] = machine.s
+
+        obj = super().__new__(mcs, name, bases, new_attrs)
+
+        for key, val in new_attrs.items():
+            if isinstance(val, Updater):
+                val.set_project(obj)
+                machine.add_updater(val)
+
+        return obj
+
+
+class Project(object, metaclass=ProjectMeta):
+    s = None
+    _machine = None
+
+    @classmethod
+    def run(cls, tick_per_mil, lasted_time, fd):
+        cls._machine._tick_per_mil = tick_per_mil
+        cls._machine.fd = fd
+
+        cls._machine._dump_sig_header()
+        for 没用 in range(int(cls._machine.calc_sim_time(lasted_time))):
+            cls._machine.update(cls._machine.get_mil_per_tick())
 
 
 # test only
