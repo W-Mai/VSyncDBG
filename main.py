3532e91e01644f3e3b39e0cb061bbc17f3dd19e5
FASTSHIFT <vifextech@foxmail.com>
2023-07-12 23:08:33 +0800

    Use pollcnt to replace the queue, support video and cmd screen simulation

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from random import randrange
 
 
 class Prj(Project):
-    INITED = False
-    VSYNC_QUEUE_LEN = 2
+    VSYNC_QUEUE_LEN = 1
     VSYNC_QUEUE = []
+    VIDEO_MODE = False
 
     pollmon = Signal(1)
+    pollcnt = Signal(1 if VIDEO_MODE else 2)
     lcd = Signal(0)
     render = Signal(0)
     render_buffer = Signal(0)
@@ -30,20 +31,20 @@ class Prj(Project):
             if not lcd.keeping(8):
                 lcd(1)
                 self.te_intr()
-
         else:
             if not lcd.keeping(8):
-                if send_buffer() > -1:
+                if send_buffer() > -1 or self.VIDEO_MODE:
                     self.frame_done()
                 lcd(0)
 
     @Updater
     def update_render(self):
         pollmon = self.s.pollmon
+        pollcnt = self.s.pollcnt
         render = self.s.render
         render_buffer = self.s.render_buffer
 
-        if pollmon() and self.get_writeable() and render() != 1:
+        if pollmon() and pollcnt() > 0 and render() != 1:
             pollmon(0)
             render(1)
             self.check_corruption()
@@ -55,17 +56,14 @@ class Prj(Project):
             if not render.keeping(idle_time):
                 pollmon(1)
         else:
-            render_time = randrange(2, 100)
+            render_time = randrange(16, 100)
             # render_time = 50
             if not render.keeping(render_time):
                 self.check_corruption()
                 self.VSYNC_QUEUE.append(render_buffer.get())
+                pollcnt(pollcnt() - 1)
                 render_buffer(1 - render_buffer.get())
                 render(0)
-
-    @Passive
-    def get_writeable(self):
-        return len(self.VSYNC_QUEUE) < self.VSYNC_QUEUE_LEN
 
     @Passive
     def get_readable(self):
@@ -73,20 +71,33 @@ class Prj(Project):
 
     @Passive
     def te_intr(self):
-        if self.get_readable():
+        if self.get_readable() or self.VIDEO_MODE:
             self.frame_start()
 
     @Passive
     def frame_start(self):
         send_buffer = self.s.send_buffer
-        send_buffer(self.VSYNC_QUEUE[0])
+        pollcnt = self.s.pollcnt
+
+        if self.get_readable():
+            send_buffer(self.VSYNC_QUEUE[0])
+            self.VSYNC_QUEUE.pop(0)
+
+            if self.VIDEO_MODE:      
+                pollcnt(pollcnt() + 1)
+
         self.check_corruption()
 
     @Passive
     def frame_done(self):
         send_buffer = self.s.send_buffer
+        pollcnt = self.s.pollcnt
+
         self.check_corruption()
-        self.VSYNC_QUEUE.pop(0)
+
+        if not self.VIDEO_MODE:      
+            pollcnt(pollcnt() + 1)
+
         send_buffer(-1)
 
     @Passive
@@ -103,7 +114,6 @@ class Prj(Project):
         if render() and lcd() and send_buffer() == render_buffer():
             corruption(corruption() + 1)
             print("corruption detected!")
-
 
 if __name__ == '__main__':
     with StringIO() as f:
